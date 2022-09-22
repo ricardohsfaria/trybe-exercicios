966de07aa3bc9f33d71f97fa0845a57642fb2b1f
Ricardo <ricardohsfaria123@gmail.com>
2022-09-22 18:18:14 -0300

    Exercicio Nova Pessoa Contratada(1)

---
+++ Fundamentos/secao-8-high-order-functions-do-JavaScript-ES6/dia-1-introducao/script.js
@@ -0,0 +1,16 @@
+const generateEmployee = (fullName) => {
+    const email = fullName.replace(' ', '_').toLowerCase();
+    return { fullName, email: `${email}@trybe.com`};
+}
+
+const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'),
+      id2: callback('Luiza Drumond'),
+      id3: callback('Carla Paiva'),
+    }
+    
+    return employees;
+  };
+
+console.log(newEmployees(generateEmployee));
